c31bd088139678a69fabcab68f50ade7d38cdaf8
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-08-02 22:16:00 +0530

    Add files via upload

---
+++ Virtual_fns_Polymorphism_Runtime.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Base{
+public:
+int var_base=100;
+ virtual void display(){
+  cout<<"varbase 1="<<var_base;
+}
+
+};
+class Derived: public Base{
+public:
+int var_der=5 ;
+void display()
+{
+    cout<<"2var_der="<<var_der<<endl;
+    cout<<"2var_base="<<var_base<<endl;
+}
+};
+int main() {
+
+  Base *base_ptr;
+  Base obj_base;
+  Derived obj_der;
+
+  base_ptr = &obj_der;
+  base_ptr -> display();
+
+
+  // Derived *der_ptr;
+  // der_ptr= &obj_der;
+ 
+  
+  
+  
+  
+  return 0;
+}
